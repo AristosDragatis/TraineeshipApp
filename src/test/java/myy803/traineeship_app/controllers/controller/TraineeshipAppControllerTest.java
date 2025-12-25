c682bf994dc115d2e2a27eed792ecabe6cde8328
AristosDragatis <dragatis.aris@gmail.com>
2025-12-25 21:50:41 +0200

    finish committee assign position test

--- src/test/java/myy803/traineeship_app/controllers/controller/TraineeshipAppControllerTest.java
+++ src/test/java/myy803/traineeship_app/controllers/controller/TraineeshipAppControllerTest.java
@@ -1,25 +1,40 @@
 package myy803.traineeship_app.controllers.controller;
 
+import myy803.traineeship_app.controllers.TraineeshipAppController;
 import myy803.traineeship_app.controllers.searchstrategies.PositionsSearchFactory;
+import myy803.traineeship_app.controllers.searchstrategies.PositionsSearchStrategy;
 import myy803.traineeship_app.controllers.supervisorsearchstrategies.SupervisorAssigmentFactory;
+import myy803.traineeship_app.domain.TraineeshipPosition;
 import myy803.traineeship_app.mappers.CompanyMapper;
 import myy803.traineeship_app.mappers.ProfessorMapper;
 import myy803.traineeship_app.mappers.StudentMapper;
 import myy803.traineeship_app.mappers.TraineeshipPositionsMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.ui.Model;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TraineeshipAppControllerTest {
 
     @Mock
-    private PositionsSearchFactory searchFactory;
+    private PositionsSearchFactory positionsSearchFactory;
 
     @Mock
-    private SupervisorAssigmentFactory assigmentFactory;
+    private PositionsSearchStrategy mockStrategy;
+
+    @Mock
+    private SupervisorAssigmentFactory supervisorAssigmentFactory;
 
     @Mock
     private StudentMapper studentMapper;
@@ -36,9 +51,49 @@ public class TraineeshipAppControllerTest {
     @Mock
     private Model model;
 
+    @InjectMocks
+    private TraineeshipAppController controller;
 
+    private String studentUsername;
+    private String strategyName;
+    List<TraineeshipPosition> mockPositions;
+
+
+    // setting up fields
+    @BeforeEach
+    void setUp(){
+        studentUsername = "aris";
+        strategyName = "interests";
+        mockPositions = new ArrayList<>();
+
+    }
+
+    // testing committe find position method
     @Test
     void testCommitteeFindPositions(){
+        // initializing
+        mockPositions.add(new TraineeshipPosition());
+
+        // stubbing
+        when(positionsSearchFactory.create(strategyName)).thenReturn(mockStrategy);
+        when(mockStrategy.search(studentUsername)).thenReturn(mockPositions);
+
+        // act
+        String viewName = controller.findPositions(studentUsername, strategyName, model);
+
+        // check
+        assertEquals("committee/available_positions", viewName);
+
+        // verification
+        verify(model).addAttribute("positions", mockPositions);
+        verify(model).addAttribute("student_username", studentUsername);
+    }
+
+    @Test
+    void testCommitteeAssignPositions(){
+
+
+
 
     }
 
