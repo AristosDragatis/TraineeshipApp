684f3500f358a2b49f7ce598b59bb02751af54cd
AristosDragatis <dragatis.aris@gmail.com>
2026-01-16 23:50:18 +0200

    fix SearchBasedOnLocation Test

--- src/test/java/myy803/traineeship_app/controllers/searchstrategies/SearchBasedOnLocationTest.java
+++ src/test/java/myy803/traineeship_app/controllers/searchstrategies/SearchBasedOnLocationTest.java
@@ -1,77 +1,43 @@
 package myy803.traineeship_app.controllers.searchstrategies;
-
 import myy803.traineeship_app.domain.Company;
 import myy803.traineeship_app.domain.Student;
 import myy803.traineeship_app.domain.TraineeshipPosition;
-import myy803.traineeship_app.mappers.CompanyMapper;
-import myy803.traineeship_app.mappers.StudentMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-
-@ExtendWith(MockitoExtension.class)
 public class SearchBasedOnLocationTest {
 
-    @Mock
-    private CompanyMapper companyMapper;
-
-    @Mock
-    private StudentMapper studentMapper;
-
-    @InjectMocks
-    private SearchBasedOnLocation strategy;
-
     @Test
-    void testSearchLocationAndFindPositions(){
-        String username = "aris";
-        String location = "Ioannina";
+    void filterPositions(){
+        // Setup data
+        SearchBasedOnLocation strategy = new SearchBasedOnLocation();
+        Student student = new Student("new_student");
+        student.setPreferredLocation("Ioannina");
 
-        Student mockStudent = new Student(username);
-        mockStudent.setPreferredLocation(location);
+        // position at Ioannina that matches
+        TraineeshipPosition pos1 = new TraineeshipPosition();
+        pos1.setCompany(new Company("owner1", "Teamviewer","Ioannina", new ArrayList<>()));
+        pos1.setAssigned(false);
 
-        // initialize an empty list to avoid errors
-        List<TraineeshipPosition> positionsList = new ArrayList<>();
-
-        // create a company at this preferred location
-        Company mockCompany = new Company("TeamViewer", "Teamviewer", location, positionsList);
-
-        // create a new position at the company
-        TraineeshipPosition  position = new TraineeshipPosition();
-        position.setTitle("Backend Dev");
-        position.setAssigned(false); // getAvailablePositions() filters the assigned to false
-        mockCompany.addPosition(position);
+        // position that does not match
+        TraineeshipPosition pos2 = new TraineeshipPosition();
+        pos2.setCompany(new Company("owner2", "Teamviewer", "Athens", new ArrayList<>()));
+        pos2.setAssigned(false);
 
 
-        // mocking behaviour
-        when(studentMapper.findByUsername(username)).thenReturn(mockStudent);
+        List<TraineeshipPosition> input = new ArrayList<>();
+        input.add(pos1);
+        input.add(pos2);
 
-        when(companyMapper.findByCompanyLocation(location)).thenReturn(Arrays.asList(mockCompany));
+        // call the strategy method
+        List<TraineeshipPosition> result = strategy.filterPositions(input, student);
 
-        // execute the search method by passing the username
-        List<TraineeshipPosition> results = strategy.search(username);
-
-
-        // check
-        assertNotNull(results);
-        assertEquals(1, results.size(), "1 position must be found");
-        assertEquals("Backend Dev", results.get(0).getTitle());
-
-
-        // verify company
-        verify(companyMapper).findByCompanyLocation(location);
-
-
+        // assertion
+        assertEquals(1, result.size());
+        assertEquals("Ioannina", result.get(0).getCompany().getCompanyLocation());
     }
 }
